d966222d70baf21c3efd4960ac4b2e8076c00930
samuele2258 <samuelecaporale@icloud.com>
2023-05-25 17:28:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vacuumStore
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dwuexw)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'BSV 33',
+    price: 400,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'BSV 33S',
+    price: 450,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'CSV 42',
+    price: 2000,
+    description: '',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
