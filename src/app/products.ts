58ad0f679fa8685efe57bcefa86b55209f08f7ad
samuele2258 <samuelecaporale@icloud.com>
2023-05-26 22:19:45 +0200

    /add products

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,19 +10,55 @@ export const products = [
     id: 1,
     name: 'BSV 33',
     price: 400,
-    description: 'A large phone with one of the best screens',
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
   },
   {
     id: 2,
     name: 'BSV 33S',
     price: 450,
-    description: 'A great phone with one of the best cameras',
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
   },
   {
     id: 3,
+    name: 'BSV 44',
+    price: 2000,
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
+  },
+  {
+    id: 4,
+    name: 'BSV 44S',
+    price: 2000,
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
+  },
+  {
+    id: 5,
+    name: 'BSV 45',
+    price: 2000,
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
+  },
+  {
+    id: 6,
+    name: 'BSV 45 DS',
+    price: 2000,
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
+  },
+  {
+    id: 7,
+    name: 'BSV 54',
+    price: 2000,
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
+  },
+  {
+    id: 8,
+    name: 'BSV 94',
+    price: 2000,
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
+  },
+  {
+    id: 8,
     name: 'CSV 42',
     price: 2000,
-    description: '',
+    description: 'Macchinario sottovuoto per ogni tipo di mercato',
   },
 ];
 
