cabaa31366b419f02eeebb10d2e4e9458e4cfc01
samuele2258 <samuelecaporale@icloud.com>
2023-05-27 15:29:35 +0200

    5 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,66 +4,72 @@ export interface Product {
   price: number;
   description: string;
 }
-
+export enum Tag {
+  BSV = 'bsv',
+  CSV = 'csv',
+}
 export const products = [
   {
     id: 1,
     name: 'BSV 33',
     price: 400,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 2,
     name: 'BSV 33S',
     price: 450,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 3,
     name: 'BSV 44',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 4,
     name: 'BSV 44S',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 5,
     name: 'BSV 45',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 6,
     name: 'BSV 45 DS',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 7,
     name: 'BSV 54',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 8,
     name: 'BSV 94',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.BSV,
   },
   {
     id: 8,
     name: 'CSV 42',
     price: 2000,
     description: 'Macchinario sottovuoto per ogni tipo di mercato',
+    type: Tag.CSV,
   },
 ];
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
